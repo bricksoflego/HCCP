39b307fb777025b603e6ae8645143c613ccd6a32
Brent Ricks <brent.ricks@igmal.com>
2024-09-23 20:09:59 -0600

    Added Validation Checks and More Unit Tests

--- BlueDragonTests/Components/AuditTests.cs
+++ BlueDragonTests/Components/AuditTests.cs
@@ -1,9 +1,6 @@
 ﻿using BlueDragon.Services;
 using Bunit;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Forms;
-using MudBlazor;
 using MudBlazor.Services;
 
 namespace BlueDragonTests.Components
@@ -29,6 +26,9 @@ namespace BlueDragonTests.Components
 
             // Register other MudBlazor services
             _ctx.Services.AddMudServices();
+
+            // Configure JSInterop to handle MudBlazor's JavaScript calls
+            _ctx.JSInterop.SetupVoid("mudKeyInterceptor.connect", _ => true);  // Allow any arguments for mudKeyInterceptor.connect
         }
 
         [TestCleanup]
@@ -44,6 +44,25 @@ namespace BlueDragonTests.Components
             var cut = _ctx.RenderComponent<BlueDragon.Components.Audit>();
 
             // Assert: Verify the presence of specific content
+        }
+
+        [TestMethod]
+        public void BarcodeValidation_WithValidBarcode_ParsesCorrectly()
+        {
+            // Arrange
+            var cut = _ctx.RenderComponent<BlueDragon.Components.Audit>();
+            var validBarcode = "112023092501"; // A valid barcode
+            var barcodeField = cut.Find("#barcode");
+
+            // Act
+            barcodeField.Change(validBarcode);
+
+            // Use reflection to get the value of the private ItemDate property
+            var itemDateField = cut.Instance.GetType().GetProperty("ItemDate", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            var itemDateValue = itemDateField?.GetValue(cut.Instance)?.ToString();
+
+            // Assert
+            Assert.AreEqual("NA", itemDateValue);
         }
 
         // A minimal AuthService mock that can be used for the test
